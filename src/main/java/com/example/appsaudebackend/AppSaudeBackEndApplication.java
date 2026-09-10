96cd04881ab58d304856715d9da0417389e21899
joaopedrofuhr45-cmd <joaopedrofuhr45@gmail.com>
2026-09-09 21:36:42 -0300

    first commit

---
+++ src/main/java/com/example/appsaudebackend/AppSaudeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.appsaudebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSaudeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSaudeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appsaudebackend/AppSaudeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appsaudebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSaudeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
